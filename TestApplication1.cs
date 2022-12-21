b93674ed3a65d4ce46d4e70816cbdefd772fe20d
SMANTA\smans <smanta.shrestha@gmail.com>
2022-12-21 16:10:53 -0500

    Added NuGet packages, chromedriver, renamed class

---
+++ TestApplication1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace calculator_automation
+{
+    [TestClass]
+    public class TestApplication1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
